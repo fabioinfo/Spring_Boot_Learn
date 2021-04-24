a843791402e5c187cd622401d6ad52a2a4b7bb00
fabioinfo <fabioinfohw@gmail.com>
2021-04-24 02:29:16 -0400

    Firsit commit: project structure created

---
+++ src/test/java/com/springboot/dev/fabioinfo/springbootlearn/SpringbootlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.dev.fabioinfo.springbootlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
